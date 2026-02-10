4e8d05cad6049a60b9951b84fc174a29f8e25b17
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-11 00:20:41 +0530

    routes

--- day-14/src/app.js
+++ day-14/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const authRouter = require("./routers/auth.route")
+const cookieparser = require("cookie-parser")
 
 const app = express()
 app.use(express.json())
+app.use(cookieparser())
+app.use("/api/auth", authRouter)
 
 module.exports = app
