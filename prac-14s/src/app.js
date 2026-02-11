8fadd352b4f4c362fa10ca276b0cc3cf84d1b07f
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-11 17:17:22 +0530

    PRACTICE Day-14 task

--- prac-14s/src/app.js
+++ prac-14s/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const authRoutes = require("../src/routes/auth.route")
+const cookieparser = require("cookie-parser")
 
 const app = express ()
 app.use(express.json())
+app.use(cookieparser())
+app.use("/api/auth", authRoutes)
 
 module.exports= app
