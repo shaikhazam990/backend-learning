7d350c1c045fc01e55ee9d7d2ba9fd068edd0b02
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-12 11:06:45 +0530

    connected to db

---
+++ prac-15/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const app = require("./src/app")
+
+const connectToDb = require("./src/config/database")
+
+connectToDb()
+
+ app.listen(3000, (req,res)=>{
+    console.log("server is running on port 3000")
+
+})
