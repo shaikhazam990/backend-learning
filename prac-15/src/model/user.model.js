4e6bf06f899cddb9bf9f356c38dd138f4ffa6166
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-12 11:13:44 +0530

    create model

---
+++ prac-15/src/model/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true, "username already exists"],
+        required:[true, "username require"]
+    },
+    email:{
+        type:String,
+        unique:[true, "email already exists"],
+        required:[true, "email require"]
+    },
+    password:{
+        type:String,
+        required:[true, "password require"],
+    },
+    bio:{
+        type:String
+    },
+    profileImage:{
+        type:String,
+        default:"https://ik.imagekit.io/gdmcmlxtw/9e837528f01cf3f42119c5aeeed1b336.jpg?updatedAt=1770747607441"
+    }
+})
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports= userModel
