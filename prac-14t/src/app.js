d22296980d835bcbe77d0590aa3c366354aa7e8e
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-12 19:35:45 +0530

    create post using imagekit

--- prac-14t/src/app.js
+++ prac-14t/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const authRouter = require("./routes/user.routes")
 const cookieparser = require("cookie-parser")
+
+const postRouter = require("./routes/post.routes")
 const app = express()
 
 app.use(express.json())
 app.use(cookieparser())
 
 app.use("/api/auth", authRouter)
+app.use("/api/posts", postRouter)
 
 module.exports=app
