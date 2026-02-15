fb5e0219c2b5aa3150082eba203d70e7284713f1
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-15 19:40:40 +0530

    create user register and login controller

--- prac-17/src/app.js
+++ prac-17/src/app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 
+const cookieparser = require("cookie-parser")
+const authRouter = require("./routes/user.route")
+
 const app = express()
+
 app.use(express.json())
+app.use(cookieparser())
+
+app.use("/api/auth", authRouter)
 
 module.exports=app
