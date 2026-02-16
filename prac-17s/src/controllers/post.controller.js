c60813175eea75fd5f8384d11ec95740c7b3bcf5
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-16 11:34:41 +0530

    CREATE Post

--- prac-17s/src/controllers/post.controller.js
+++ prac-17s/src/controllers/post.controller.js
@@ -4,6 +4,8 @@ const ImageKit = require("@imagekit/nodejs")
 
 const {toFile} = require("@imagekit/nodejs")
 
+const jwt = require("jsonwebtoken")
+
 const client = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 });
@@ -11,18 +13,54 @@ const client = new ImageKit({
 async function createPostController(req,res){
     console.log(req.body, req.file)
 
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"invalid token"
+        })
+    }
+
+    let decode;
+
+    try {
+        decode=jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+
+        return res.status(409).json({
+            message:"unauthorized process"
+        }) 
+    }
+    console.log(decode)
+
   const files =   await client.files.upload({
   file: await toFile(Buffer.from(req.file.buffer), 'file'),
   fileName: 'test',
+  folder: "prac-insta-clone",
 });
 
-        res.send(files)
+        const post = await postModel.create({
+            caption:req.body.caption,
+            imgUrl:files.url,
+            user:decode.id
+        })
+
+        return res.status(201).json({
+            message:"post Created successfully"
+        })
+
+
+
+        // res.send(files)
 
 
 
 
 }
 
+
+
 module.exports={
-    createPostController
+    createPostController,
+    
 }
