f26f6195a629ff6117af4eee8a15943943d49ac8
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-19 16:58:01 +0530

    setup login frontend

--- day-19/Frontend/src/features/auth/pages/Login.jsx
+++ day-19/Frontend/src/features/auth/pages/Login.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React , {useState} from 'react'
 import '../style/form.scss'
 import { Link } from 'react-router'
 import Register from './register'
+import axios from 'axios'
 const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handlesubmit(e){
+        e.preventDefault()
+
+        axios.post("http://localhost:3000/api/auth/login",{
+            username,
+            password
+        },{withCredentials:true})
+        .then((res)=>{
+            console.log(res.data)
+        })
+    }
   return (
     <main>
         <div className="form-container">
             <h1>Login-Form</h1>
-            <form>
+            <form onSubmit={handlesubmit}>
                 <input 
+                    onInput={(e)=>{setUsername(e.target.value)}}
                     type="text"
                     name='username' 
                     placeholder='Enter username' />
                 <input 
+                    onInput={(e)=>{setPassword(e.target.value)}}
                     type="text" 
                     name='password' 
                     placeholder='Enter Password' />
