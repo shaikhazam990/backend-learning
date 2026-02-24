d2ba40727445eb8e7e16e5e2ac1c17dc726cac41
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-24 16:21:57 +0530

    create post context

--- day-21/Frontend/src/App.jsx
+++ day-21/Frontend/src/App.jsx
@@ -3,12 +3,15 @@ import { RouterProvider } from 'react-router'
 import {router} from './app.routes'
 import { AuthProvider } from './features/auth/auth.context'
 import './features/shared/globle.scss'
+import { PostContextProvider } from './features/post/post.context'
 
 
 const App = () => {
   return (
     <AuthProvider>
-    <RouterProvider router={router}/>
+      <PostContextProvider>
+        <RouterProvider router={router}/>
+      </PostContextProvider>
     </AuthProvider>
 
   )
--- day-21/Frontend/src/features/post/services/post.api.js
+++ day-21/Frontend/src/features/post/services/post.api.js
@@ -1,2 +1,14 @@
 import axios from "axios";
 
+const api = axios.create({
+    baseURL:"http://localhost:3000",
+    withCredentials:true
+})
+
+export async function getFeed() {
+
+    const response = await api.get("/posts/feed")
+
+    return response.data
+    
+}
