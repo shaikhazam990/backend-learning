720a2a3672d9752309ec0be99edbdde8764dc024
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-26 17:07:15 +0530

    implement mediapipe

---
+++ day-25/Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FaceExpression from './features/expression/pages/FaceExpression'
+
+const App = () => {
+  return (
+    <FaceExpression/>
+  )
+}
+
+export default App
