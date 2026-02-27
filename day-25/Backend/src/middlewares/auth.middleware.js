f558274c8968539fb565aff4526b0f07710fdf1d
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-28 01:11:26 +0530

    create middlewares

--- day-25/Backend/src/middlewares/auth.middleware.js
+++ day-25/Backend/src/middlewares/auth.middleware.js
@@ -1,4 +1,38 @@
 const blacklistModel = require("../model/blacklist.model")
-
 const userModel = require("../model/user.model")
-const jwt = require("json")
+const jwt = require("jsonwebtoken")
+
+async function authUser(req,res,next) {
+    const token = req.cookie.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"token not provided"
+        })
+    }
+
+    const istokenBlacklisted = await blacklistModel.findOne({
+        token
+    })
+
+    if(istokenBlacklisted){
+        return res.status(401).json({
+            message:"invalid token"
+        })
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        req.user=decode
+        next()
+    } catch (error) {
+        return res.status(401).json({
+            message:"invalid token"
+        })   
+    }
+}
+
+module.exports={
+    authUser
+}
+
