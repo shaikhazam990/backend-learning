39565fd56b4049b01b262d829ff1f998042dd49f
shaikhazam990 <shaikhazam0990@gmail.com>
2026-03-04 02:34:41 +0530

    created uploadSong

--- day-25/Backend/src/controller/song.controller.js
+++ day-25/Backend/src/controller/song.controller.js
@@ -1,10 +1,43 @@
 const songModel = require("../model/song.model")
+const storageService = require("../services/storage.service")
+const id3 = require("node-id3")
 
 
-async function uploadSong(req,res) {
-    console.log(req.file)
-    
+async function uploadSong(req, res) {
+
+    const songBuffer = req.file.buffer
+    const { mood } = req.body
+
+    const tags = id3.read(songBuffer)
+
+    const [ songFile, posterFile ] = await Promise.all([
+        storageService.uploadFile({
+            buffer: songBuffer,
+            filename: tags.title + ".mp3",
+            folder: "/cohort-2/moodify/songs"
+        }),
+        storageService.uploadFile({
+            buffer: tags.image.imageBuffer,
+            filename: tags.title + ".jpeg",
+            folder: "/cohort-2/moodify/posters"
+        })
+    ])
+
+    const song = await songModel.create({
+        title: tags.title,
+        url: songFile.url,
+        posterUrl: posterFile.url,
+        mood
+    })
+
+    res.status(201).json({
+        message: "song created successfully",
+        song
+    })
+
 }
+    
+
 
 module.exports={
     uploadSong
--- day-25/Backend/src/model/song.model.js
+++ day-25/Backend/src/model/song.model.js
@@ -12,6 +12,14 @@ const songSchema = new mongoose.Schema({
     title:{
         type:String,
         required:true
+    },
+    mode:{
+        type:String,
+        enum:{
+            values:["sad", "happy", "surprised"],
+            message:"enum this is "
+        }
+
     }
 })
 
