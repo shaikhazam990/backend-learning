6330c5949b2fd6988cdabeee92bd1cd050e1840c
shaikhazam990 <shaikhazam0990@gmail.com>
2026-03-07 03:45:17 +0530

    ..

--- day-25/Backend/src/controller/auth.controller.js
+++ day-25/Backend/src/controller/auth.controller.js
@@ -1,120 +1,121 @@
-const userModel = require("../model/user.model")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const blacklistModel = require("../model/blacklist.model")
+const userModel = require("../model/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const blacklistModel = require("../model/blacklist.model");
 
-const redis = require("../config/cache")
+const redis = require("../config/cache");
 
-async function registerController(req,res){
-    const{username, email, password} = req.body
+async function registerController(req, res) {
+  const { username, email, password } = req.body;
 
-    const isAllExist = await userModel.findOne({
-        $or:[
-            {username},
-            {email}
-        ]
-    })
-    
-    if(isAllExist){
-        return res.status(400).json({
-            message:isAllExist.email==email ? "email already exist" : "username already exists"
-        })
-    }
-    const hash = await bcrypt.hash(password,10)
+  const isAllExist = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
 
-    const user = await userModel.create({
-        username,email,password:hash
-    })
+  if (isAllExist) {
+    return res.status(400).json({
+      message:
+        isAllExist.email == email
+          ? "email already exist"
+          : "username already exists",
+    });
+  }
+  const hash = await bcrypt.hash(password, 10);
 
-    const token = jwt.sign(
-        {id:user._id,
-        username:user.username
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" },
+  );
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  });
+
+  return res.status(201).json({
+    message: "user registered successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
     },
-        process.env.JWT_SECRET,
-        {expiresIn: "3d"}
-    )
-    res.cookie("token", token)
+  });
+}
 
-    return res.status(201).json({
-        message:"user registered successfully",
-        user:{
-            id:user._id,
-            username:user.username,
-            email:user.email
-        }
+async function loginController(req, res) {
+  const { email, username, password } = req.body;
+
+  const user = await userModel
+    .findOne({
+      $or: [{ username }, { email }],
     })
+    .select("+password");
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid credentials",
+    });
+  }
+
+  const passwordValid = await bcrypt.compare(password, user.password);
+
+  if (!passwordValid) {
+    return res.status(400).json({
+      message: "Invalid credentials",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "3d" },
+  );
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  });
+
+  return res.status(201).json({
+    message: "user loggedIn successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
 }
 
-async function loginController(req,res){
-    const {email,username, password} = req.body
+async function getMeController(req, res) {
+  const user = await userModel.findById(req.user.id);
 
-    const user = await userModel.findOne({
-        $or:[
-            {username},
-            {email}
-        ]
-    }).select("+password")
-
-    if(!user){
-        return res.status(400).json({
-            message:"Invalid credentials"
-        })
-    }
-
-    const passwordValid = await bcrypt.compare(password,user.password)
-
-    if(!passwordValid){
-        return res.status(400).json({
-            message:"Invalid credentials"
-        })
-    }
-
-    const token = jwt.sign(
-        {id:user._id,
-            username:user.username
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:"3d"}
-    )
-    res.cookie("token", token)
-
-
-    return res.status(201).json({
-        message:"user loggedIn successfully",
-        user:{
-            id:user._id,
-            username:user.username,
-            email:user.email
-        }
-    })
-    
+  res.status(200).json({
+    message: "user fetched successfully",
+    user,
+  });
 }
 
-async function getMeController(req,res){
-    const user = await userModel.findById(req.user.id)
+async function logoutController(req, res) {
+  const token = req.cookies.token;
+  res.clearCookie("token");
 
-    res.status(200).json({
-        message:"user fetched successfully",
-        user
-    })
-    
+  await redis.set(token, Date.now().toString(), "EX", 60 * 60);
+
+  res.status(200).json({
+    message: "logout Successfully",
+  });
 }
 
-async function logoutController(req,res){
-    const token = req.cookies.token
-    res.clearCookie("token")
-
-
-    await redis.set(token, Date.now().toString(), "EX", 60*60)
-
-    res.status(200).json({
-        message:"logout Successfully"
-    })    
-}
-
-module.exports={
-    registerController,
-    loginController,
-    getMeController,
-    logoutController
-}
+module.exports = {
+  registerController,
+  loginController,
+  getMeController,
+  logoutController,
+};
