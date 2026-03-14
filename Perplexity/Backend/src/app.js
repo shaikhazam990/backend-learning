ec8ea2e2995c6a42368b832864d43a601ef63c89
shaikhazam990 <shaikhazam0990@gmail.com>
2026-03-15 03:59:00 +0530

    use morgan

--- Perplexity/Backend/src/app.js
+++ Perplexity/Backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import morgan from "morgan"
+import cors from cors
 
 const app = express();
 
@@ -8,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan("dev"))
+app.use(cors({
+    origin:"http://localhost:5173",
+    Credential:true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+
+
+}))
 
 // Health check
 app.get("/", (req, res) => {
--- Perplexity/Frontend/src/app/App.jsx
+++ Perplexity/Frontend/src/app/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { RouterProvider } from 'react-router'
-import {router} from './app.routes'
-const App = () => {
-  return (
-    <RouterProvider router={router}/>
-  )
-}
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app.routes";
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
